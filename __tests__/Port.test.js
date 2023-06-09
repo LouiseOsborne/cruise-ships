f407b7980bbb3627b6626aba4c2a95a3089b7513
Louise <lkosborne@live.com>
2023-06-09 13:42:13 +0100

    tests for Ports have ships

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -4,12 +4,33 @@ const Port = require('../src/Port.js');
 describe('Port', () => {
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
-    });
+    }),
 
     it('has a name', () => {
         const port = new Port('Southampton');
 
         expect(port.portName).toBe('Southampton');
-    })
+    }),
+
+    it('can add a ship', () => {
+        const port = new Port('Dover');
+        const ship = {};
+        
+        port.addShip(ship);
+
+        expect(port.ships).toContain(ship);
+    }),
+
+    it('can remove a ship', () => {
+        const port = new Port('Dover');
+        const titanic = {};
+        const queenMary = {};
+
+        port.addShip(titanic);
+        port.addShip(queenMary);
+        port.removeShip(queenMary);
+
+        expect(port.ships).toEqual([titanic]);
+    });
 
 });
